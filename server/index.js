2318f3ab10c2ee63bdac3123d1c9d56e668815c8
Mustafa <mustafamfaisal@gmail.com>
2023-05-07 19:05:05 +0300

    connect to DB
    Create a cluster and Connect to the DB , create routes and schema for posts

--- server/index.js
+++ server/index.js
@@ -1,5 +1,22 @@
-import { Express } from "express";
+import express  from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+
+import postRoutes from "./routes/posts.js";
+
+const app = new express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json( {limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded( {limit: "30mb", extended: true}));
+app.use(cors());
+
+const COONECTION_URL = 'mongodb+srv://mustafa_alfaisal:PRNJURQw26r3SqPV@cluster0.mrhsv1n.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(COONECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
 
